164256a51c3eafda547e8f2823556251f82b687c
SenecatLoic <loic.senecat@hotmail.fr>
2021-12-10 15:48:22 +0100

    Finished Locations, started Dashboard

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,76 @@
-import './App.css';
-import { Header } from './component/Header';
+import { styled } from "@mui/material";
+import "./App.css";
+import { Header } from "./component/Header";
+import ChartJS from "chart.js/auto";
+import { Doughnut, Line, Bar } from "react-chartjs-2";
+import "chartjs-adapter-moment";
+ChartJS.register();
 
+const StyledContainer = styled("div")({
+  display: "flex",
+  justifyContent: "center",
+  width: "100%",
+});
 
+const StyledStack = styled("div")({
+  display: "flex",
+  flexDirection: "column",
+  width: "75%",
+});
 
+const StyledTitle = styled("div")({
+  fontSize: "calc(0.5em + 1vw)",
+  fontWeight: "bold",
+  marginTop: "5%",
+});
 
 function App() {
-  const client = new WebSocket('ws://localhost:3000');
+  const client = new WebSocket("ws://localhost:3000");
   client.addEventListener("open", () => {
     // Causes the server to print "Hello"
-    client.send('Hello');
-  })
-  client.addEventListener("message", msg => {
+    client.send("Hello");
+  });
+
+  client.addEventListener("message", (msg) => {
     const data = JSON.parse(msg.data);
     console.log(data);
-  })
-
+  });
+  const survey = {
+    id: "rqueu87878",
+    name: "SIDA",
+    dtcreation: 8248768732,
+    dtend: 9284938578735,
+    survey: {
+      question: "Avez-vous le sida ?",
+      answers: ["oui", "non", "peut-etre"],
+    },
+    data: [
+      { device: "oue8495", answerId: 2, dtanswer: 43573953545 },
+      { device: "coeuouoe", answerId: 1, dtanswer: 43573993545 },
+      { device: "ouoeurch", answerId: 0, dtanswer: 43579993545 },
+    ],
+  };
 
   return (
     <div>
-      <Header/>
+      <Header />
+      <StyledContainer>
+        <StyledStack>
+          <StyledTitle>Dashboard</StyledTitle>
+          <Bar
+            data={{
+              labels: survey.survey.answers,
+              datasets: [
+                {
+                  label: "Votes",
+                  data: [1, 2, 3],
+                },
+              ],
+            }}
+            options={{ parsing: false }}
+          />
+        </StyledStack>
+      </StyledContainer>
     </div>
   );
 }
--- client/src/component/Location.tsx
+++ client/src/component/Location.tsx
@@ -1,10 +1,36 @@
 import { Header } from "./Header";
-import { Table, TableBody, TableCell, TableRow, IconButton, Input } from "@mui/material";
-import { useState, useEffect } from "react"
-import { getLocations, updateLocation } from "../Provider/Location"
-import EditIcon from '@mui/icons-material/Edit';
-import SaveIcon from '@mui/icons-material/Save';
-import CloseIcon from '@mui/icons-material/Close';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableRow,
+  IconButton,
+  Input,
+  styled,
+} from "@mui/material";
+import { useState, useEffect } from "react";
+import { getLocations, updateLocation } from "../Provider/Location";
+import EditIcon from "@mui/icons-material/Edit";
+import SaveIcon from "@mui/icons-material/Save";
+import CloseIcon from "@mui/icons-material/Close";
+
+const StyledContainer = styled("div")({
+  display: "flex",
+  justifyContent: "center",
+  width: "100%",
+});
+
+const StyledStack = styled("div")({
+  display: "flex",
+  flexDirection: "column",
+  width: "75%",
+});
+
+const StyledTitle = styled("div")({
+  fontSize: "calc(0.5em + 1vw)",
+  fontWeight: "bold",
+  marginTop: "5%",
+});
 
 export function Location() {
   const [locations, setLocations]: any[] = useState([]);
@@ -14,7 +40,11 @@ export function Location() {
       if (locations.length) {
         //à faire
       }
-      locations = locations.map((location: any) => ({ ...location, editedName: location.name, isEdit: false }))
+      locations = locations.map((location: any) => ({
+        ...location,
+        editedName: location.name,
+        isEdit: false,
+      }));
       setLocations(locations);
     })();
   }, []);
@@ -22,53 +52,78 @@ export function Location() {
   const editMode = (location: any) => (
     <TableRow key={location.id}>
       <TableCell>{location.id}</TableCell>
-      <TableCell><Input defaultValue={location.name} onChange={(e) => {
-        location.editedName = e.target.value;
-        setLocations([...locations]);
-      }} /></TableCell>
-      <TableCell><IconButton color={"primary"} onClick={async () => {
-        const result = await updateLocation(location);
-        location.isEdit = false;
-        if (result && !result.error) {
-          location.name = location.editedName;
-        }
-        else {
-          location.editedName = location.name;
-        }
-        setLocations([...locations]);
-      }}><SaveIcon /></IconButton>
-        <IconButton color={"secondary"} onClick={() => {
-          location.isEdit = false;
-          location.editedName = location.name;
-          setLocations([...locations]);
-        }}><CloseIcon /></IconButton>
+      <TableCell>
+        <Input
+          defaultValue={location.name}
+          onChange={(e) => {
+            location.editedName = e.target.value;
+            setLocations([...locations]);
+          }}
+        />
+      </TableCell>
+      <TableCell>
+        <IconButton
+          color={"primary"}
+          onClick={async () => {
+            const result = await updateLocation(location);
+            location.isEdit = false;
+            if (result && !result.error) {
+              location.name = location.editedName;
+            } else {
+              location.editedName = location.name;
+            }
+            setLocations([...locations]);
+          }}
+        >
+          <SaveIcon />
+        </IconButton>
+        <IconButton
+          color={"secondary"}
+          onClick={() => {
+            location.isEdit = false;
+            location.editedName = location.name;
+            setLocations([...locations]);
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
       </TableCell>
     </TableRow>
-  )
-
+  );
 
   const watchMode = (location: any) => (
     <TableRow key={location.id}>
       <TableCell>{location.id}</TableCell>
       <TableCell>{location.name}</TableCell>
-      <TableCell><IconButton color={"primary"} onClick={() => {
-        location.isEdit = true;
-        setLocations([...locations]);
-      }}><EditIcon /></IconButton></TableCell>
+      <TableCell>
+        <IconButton
+          color={"primary"}
+          onClick={() => {
+            location.isEdit = true;
+            setLocations([...locations]);
+          }}
+        >
+          <EditIcon />
+        </IconButton>
+      </TableCell>
     </TableRow>
-  )
+  );
 
   return (
     <div>
       <Header />
-      <h1>Locations</h1>
-      <Table sx={{ maxWidth: "75%" }}>
-        <TableBody>
-          {locations.map((location: any) => (
-            location.isEdit ? editMode(location) : watchMode(location)
-          ))}
-        </TableBody>
-      </Table>
+      <StyledContainer>
+        <StyledStack>
+          <StyledTitle>Locations</StyledTitle>
+          <Table sx={{ maxWidth: "75%" }}>
+            <TableBody>
+              {locations.map((location: any) =>
+                location.isEdit ? editMode(location) : watchMode(location)
+              )}
+            </TableBody>
+          </Table>
+        </StyledStack>
+      </StyledContainer>
     </div>
   );
 }
