5dba09b02d89e92bdec1d4fcee8e153d54001ca9
SenecatLoic <loic.senecat@hotmail.fr>
2022-03-17 15:18:05 +0100

    fix front end

--- web_server/index.js
+++ web_server/index.js
@@ -299,8 +299,8 @@ server.on("upgrade", (req, socket, head) => {
   });
 });
 
-app.use(express.static(path.join(__dirname, "client", "build")));
+app.use(express.static(path.join(__dirname, "../../client", "build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "../../client", "build", "index.html"));
 });
